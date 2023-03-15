116748594f45b2eddee851709619509f2f5e465b
bucuc <unuoarecare19@gmail.com>
2023-03-16 00:13:01 +0200

    updated some stuff -moved from bulma to tailwind css -fixed the Google Maps div, so it is responsive -implemented capacitor and now the project has native android compatibility

--- src/Dev Guidelines and Notes/TODO LIST.md
+++ src/Dev Guidelines and Notes/TODO LIST.md
@@ -1,5 +1,8 @@
-- [/]  <strong>Create the tourist destination tab and bus routes tab, only a proof of concept for now.</strong> 🔼 🛫 2023-03-01 📅 2023-03-02
-- [ ] Compile an .json file with all the routes an waypoints in order.
+- [ ] <strong>Create the tourist destination tab and bus routes tab, only a proof of concept for now.</strong> 🔼 🛫 2023-03-01 📅 2023-03-02
+- [x] Compile an .json file with all the routes an waypoints in order. ✅ 2023-03-13
+- [x] Export app to android. ✅ 2023-03-13
+- [ ] Settle on a CSS Framework (Tailwind maybe?).
+- [ ] Install Obsidian on mobile, sort things out while on the way.
 
 <h2><strong>Nice to have(s)</strong></h2>
-- [?] Lines of different color for every different Google Maps bus route.
+- [ ] Lines of different color for every different Google Maps bus route.
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-//import {default} from "snazzy-info-window";
+//fisierul json cu rutele
+let responseData=null;
 let start={lat:46.64333225279801,lng: 27.719046965119666};
 //46.64268725956498, 27.71892655823447
 let end = {lat: 46.647368534350484, lng:27.747928991790555};
 let directionsService;
 let directionsRenderer;
 let map;
+
+
+function fetchData() {
+    return fetch('http://localhost/routes.json')
+        .then(response => response.json())
+        .then(data => {
+            // assign data to global variable
+            responseData = data;
+
+            // return data to continue Promise chain
+            return data;
+        })
+        .catch(error => console.error(error));
+}
+
+fetchData()
+    .then(data => {
+        // use responseData variable here, after fetchData() has completed
+        console.log(responseData);
+    })
+    .then(
+
+    )
+    .catch(error => console.error(error));
+
 function initMap() {
 
     const procoloco={ lat: 46.64268725956498, lng: 27.71892655823447 };
     map = new window.google.maps.Map(document.getElementById("map"), {
         zoom: 15,
         center: procoloco, // proco loco.
+        disableDefaultUI: true,
         styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]}
             ,{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]}
             ,{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},
@@ -21,8 +48,13 @@ function initMap() {
     directionsService = new google.maps.DirectionsService();
     directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
-    calcRoute1();
-    //let proco=new google.maps.Marker({position:start , map , title:"lmao"});
+    map.addListener("resize", () => { map.getViewPort().resize();});
+    addMarkers();
+
+}
+function addMarkers()
+{
+
     const contentString =
         '<div id="content">' +
         '<div id="siteNotice">' +
@@ -41,7 +73,7 @@ function initMap() {
     const marker = new google.maps.Marker({
         position: start,
         map,
-        title: "Uluru (Ayers Rock)",
+        title: "procoloco",
     });
 
     marker.addListener("click", () => {
@@ -51,10 +83,7 @@ function initMap() {
         });
     });
 
-    //infoWindow.open();
-
 }
-
 /*
 -----------------------------------------------
 -------MAY BE USEFUL IN THE FUTURE------------
@@ -76,62 +105,50 @@ function computeTotalDistance(result) {
 }
 
  */
-function calcRoute1() {
-    let start={lat:46.64333225279801,lng: 27.719046965119666};
-    let end = {lat: 46.647368534350484, lng:27.747928991790555};
+function calculateRoute(index, responseData)
+{
     const waypts=[];
-    waypts.push({location:{lat:46.63687946991397, lng:27.725226774689794}, stopover: true})
-    waypts.push({location:{lat:46.630101745765636, lng:27.73462523507763}, stopover: false})
-    waypts.push({location: {lat:46.63767505989159, lng:27.73874510812407},
-                stopover:true});
+    for(let i=0;i<responseData.routes[index].waypoints.length;i++)
+        waypts.push(
+            {
+                location:
+                    {
+                        lat:parseFloat(responseData.routes[index].waypoints[i].lat),
+                        lng:parseFloat(responseData.routes[index].waypoints[i].lng
+                        )},
+                stopover: false
+            });
+    const start = new google.maps.LatLng(parseFloat(responseData.routes[index].startlat),
+        parseFloat(responseData.routes[index].startlng));
+    const end = new google.maps.LatLng(parseFloat(responseData.routes[index].endlat),
+        parseFloat(responseData.routes[index].endlng));
     let request = {
         origin: start,
-        destination:end,
+        destination: end,
         travelMode: 'DRIVING',
         optimizeWaypoints: true,
-        provideRouteAlternatives:true,
         waypoints: waypts
     };
-    directionsService.route(request, function(result, status) {
+    directionsService.route(request, function (result, status) {
         if (status === 'OK') {
             directionsRenderer.setDirections(result);
         }
     });
+    console.log(directionsRenderer);
 }
-function routeChangeTest()
+
+function onRouteChange()
 {
 
     let e = document.getElementById("selector");
     let str = e.options[e.selectedIndex].value;
-    //console.log(str+"\n");
-
-    if(str==1)
-        calcRoute1();
-    else {
-        let start = {lat: 46.674914738520656, lng: 27.741164460646697};
-        let end = {lat: 46.634043243350995, lng: 27.733819231258618};
-        const waypts = [];
-        waypts.push({location: {lat: 46.647931969882755, lng: 27.737316831814788}, stopover: true})
-        waypts.push({location: {lat: 46.64884892008684, lng: 27.727274641262685}, stopover: false})
-
-        let request = {
-            origin: start,
-            destination: end,
-            travelMode: 'DRIVING',
-            optimizeWaypoints: true,
-            waypoints: waypts
-        };
-        directionsService.route(request, function (result, status) {
-            if (status === 'OK') {
-                directionsRenderer.setDirections(result);
-            }
-        });
-    }
+    fetchData().then((responseData) => calculateRoute(str-1, responseData));
 
 }
 
-
 window.initMap = initMap;
+
+
 
 
 
